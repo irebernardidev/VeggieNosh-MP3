2bf96b01bcb7eeff12094bf92b321f8fedaaf625
Ire <123773847+irebernardidev@users.noreply.github.com>
2023-10-26 18:40:53 +0100

    correct indentation of run.py

--- run.py
+++ run.py
@@ -2,14 +2,19 @@ import os
 from flask import Flask, render_template, url_for, flash, redirect
 from flask_pymongo import PyMongo, pymongo
 from bson.objectid import ObjectId
+
 if os.path.exists("env.py"):
-   import env
+    import env
+
 app = Flask(__name__)
+
 # Config Settings & Environmental Variables located in env.py
 app.config['MONGODB_NAME'] = os.environ.get('MONGODB_NAME')
 app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+
 mongo = PyMongo(app)
+
 # MongoDB Collections
 users_coll = mongo.db.users
 recipes_coll = mongo.db.recipes
@@ -24,4 +29,4 @@ def all_recipes():
     return render_template("all_recipes.html", recipes=recipes_coll.find())
 
 if __name__ == '__main__':
-    app.run(host=os.getenv('IP'), port=os.getenv('PORT'), debug=True) 
+    app.run(host=os.getenv('IP'), port=os.getenv('PORT'), debug=True)
