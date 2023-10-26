870944341efc4e2e454437a963e07788d4d693b0
Ire <123773847+irebernardidev@users.noreply.github.com>
2023-10-26 19:23:31 +0100

    create function for login, register and home

--- run.py
+++ run.py
@@ -3,12 +3,13 @@ from flask import Flask, render_template, url_for, flash, redirect
 from flask_pymongo import PyMongo, pymongo
 from bson.objectid import ObjectId
 
+# Check for environment file and import
 if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__)
 
-# Config Settings & Environmental Variables located in env.py
+# Configuration settings from environmental variables
 app.config['MONGODB_NAME'] = os.environ.get('MONGODB_NAME')
 app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
@@ -22,11 +23,30 @@ categories_coll = mongo.db.categories
 diets_coll = mongo.db.diets
 dishes_coll = mongo.db.dishes
 
-# Routes
 @app.route('/')
+@app.route("/home")
+def home():
+    """Landing page route."""
+    return render_template('home.html', title='Home')
+
 @app.route('/all_recipes')
 def all_recipes():
+    """Display all recipes route."""
     return render_template("all_recipes.html", recipes=recipes_coll.find())
 
-if __name__ == '__main__':
-    app.run(host=os.getenv('IP'), port=os.getenv('PORT'), debug=True)
+@app.route("/login")
+def login():
+    """Login route."""
+    return render_template('login.html', title='Login')
+
+@app.route("/register")
+def register():
+    """Registration route."""
+    return render_template('register.html', title='Register')
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("IP"),
+        port=int(os.environ.get("PORT")),
+        debug=True
+    )
