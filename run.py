4c29e4c6469e3381b7f51f057bd1a81545bb5e32
Ire <123773847+irebernardidev@users.noreply.github.com>
2023-10-26 18:00:43 +0100

    add package.json, .gitignore file, add Flask basic structure
    create requirements.txt file, add Procfile add config for connection to MongoDB

---
+++ run.py
@@ -0,0 +1,27 @@
+import os
+from flask import Flask, render_template, url_for, flash, redirect
+from flask_pymongo import PyMongo, pymongo
+from bson.objectid import ObjectId
+if os.path.exists("env.py"):
+   import env
+app = Flask(__name__)
+# Config Settings & Environmental Variables located in env.py
+app.config['MONGODB_NAME'] = os.environ.get('MONGODB_NAME')
+app.config['MONGO_URI'] = os.environ.get('MONGO_URI')
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+mongo = PyMongo(app)
+# MongoDB Collections
+users_coll = mongo.db.users
+recipes_coll = mongo.db.recipes
+categories_coll = mongo.db.categories
+diets_coll = mongo.db.diets
+dishes_coll = mongo.db.dishes
+
+# Routes
+@app.route('/')
+@app.route('/all_recipes')
+def all_recipes():
+    return render_template("all_recipes.html", recipes=recipes_coll.find())
+
+if __name__ == '__main__':
+    app.run(host=os.getenv('IP'), port=os.getenv('PORT'), debug=True) 
