031e3122b3542e31c8dc02d89f5dc71381e57d97
Ire <123773847+irebernardidev@users.noreply.github.com>
2023-10-27 13:43:08 +0100

    connect register function to DB users and add logout function

--- veggienosh/routes.py
+++ veggienosh/routes.py
@@ -1,51 +1,70 @@
 # Import required libraries, modules, and forms
-
-from flask import Flask, render_template, url_for, flash, redirect
-from veggienosh import app
-from veggienosh import mongo
+from flask import render_template, url_for, flash, redirect, request, session
+from veggienosh import app, mongo
+from werkzeug.security import generate_password_hash, check_password_hash
 from veggienosh.forms import RegistrationForm, LoginForm
 from flask_pymongo import PyMongo, pymongo
 from bson.objectid import ObjectId
 
-# Define MongoDB collections
+# MongoDB Collections Initialization
 users_coll = mongo.db.users
 recipes_coll = mongo.db.recipes
 categories_coll = mongo.db.categories
 diets_coll = mongo.db.diets
 dishes_coll = mongo.db.dishes
 
-# Define routes
+# Landing Page Route
 @app.route('/')
 @app.route("/home")
 def home():
     """Landing page route."""
     return render_template('home.html', title='Home', recipes=recipes_coll.find())
 
+# Route to Display All Recipes
 @app.route('/all_recipes')
 def all_recipes():
     """Display all recipes route."""
-    return render_template("all_recipes.html", recipes=recipes_coll.find(), title='Recipes')
+    return render_template("all_recipes.html", recipes=recipes_coll.find(), title='All Vegan Recipes')
 
+# User Login Route
 @app.route("/login", methods=['GET', 'POST'])
 def login():
-    """Login route."""
+    """User login route."""
     form = LoginForm()
     if form.validate_on_submit():
-        # Check credentials (This is just a placeholder; real apps should validate against the database)
-        if form.username.data == 'admin' and form.password.data == 'password':
-            flash('You have been successfully logged in!')
+        if form.username.data == 'admin' and form.password.data == 'password':  # This is a placeholder; real apps should validate against the database
+            flash('Welcome back, vegan chef! You have successfully logged in.')
             return redirect(url_for('home'))
         else:
-            flash('Username or password is incorrect. Please try again')
+            flash('Oops! Your credentials seem to be stir-fried. Please try again.')
     return render_template('login.html', form=form, title='Login')
 
+# User Registration Route
 @app.route("/register", methods=['GET', 'POST'])
 def register():
-    """Registration route."""
+    if 'username' in session:
+        return redirect(url_for('home'))
     form = RegistrationForm()
     if form.validate_on_submit():
-        # Placeholder for creating a new user (This should save the user to the database in real apps)
-        flash(f'Account created for {form.username.data}!')
-        return redirect(url_for('home'))
-    return render_template('register.html', form=form, title='Register')
+        registered_user = users_coll.find_one({'username': request.form['username']})
+        if registered_user:
+            flash("Oh snap! That username is already taken. Maybe try something more flavorful?")
+            return redirect(url_for('register'))
+        else:
+            hashed_password = generate_password_hash(request.form['password'])
+            new_user = {
+                "username": request.form['username'],
+                "password": hashed_password,
+                "user_recipes": [],
+            }
+            users_coll.insert_one(new_user)
+            session["username"] = request.form['username']
+            flash(f'Welcome to the VeggieNosh family! Your account is ready to simmer.')
+            return redirect(url_for('home'))
+    return render_template('register.html', form=form, title='Join VeggieNosh')
 
+
+@app.route("/logout")
+def logout():
+    session.pop("username",  None)
+    return redirect(url_for("home"))
