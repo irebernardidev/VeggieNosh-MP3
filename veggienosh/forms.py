09ce5a06cbcfbb9fd112260b2cec575367bd8165
Ire <123773847+irebernardidev@users.noreply.github.com>
2023-10-28 10:51:42 +0100

    Implemented change_username functionality and added corresponding template.

--- veggienosh/forms.py
+++ veggienosh/forms.py
@@ -1,9 +1,8 @@
-# Import necessary libraries and modules
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, EqualTo
 
-# --- Registration Form ---
+# --- Register Form ---
 class RegisterForm(FlaskForm):
     username = StringField('Username',
                            validators=[DataRequired(), Length(min=3, max=15)])
@@ -11,9 +10,7 @@ class RegisterForm(FlaskForm):
                              validators=[DataRequired(), Length(min=3, max=15)])
     confirm_password = PasswordField('Confirm Password',
                                      validators=[DataRequired(), EqualTo('password')])
-    # Define submit button
     submit = SubmitField('Register')
-
 
 # --- Login Form ---
 class LoginForm(FlaskForm):
@@ -22,22 +19,20 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Login')
 
-
 # --- Change Username Form ---
-class ChangeUsernameForm(FlaskForm):  # Corrected the class name here
-    old_username = StringField('Current Username',
-                               validators=[DataRequired(), Length(min=3, max=15)])
+class ChangeUsernameForm(FlaskForm):
+    # old_username = StringField('Current Username',
+    #                            validators=[DataRequired(), Length(min=3, max=15)])
     new_username = StringField('New Username',
                                validators=[DataRequired(), Length(min=3, max=15)])
     submit = SubmitField('Change Username')
-
 
 # --- Change Password Form ---
 class ChangePasswordForm(FlaskForm):
     old_password = PasswordField('Current Password',
                                  validators=[DataRequired()])
-    new_password = PasswordField('New Password', 
+    new_password = PasswordField('New Password',
                                  validators=[DataRequired()])
     confirm_new_password = PasswordField('Confirm New Password',
-                                         validators=[DataRequired(), EqualTo('new_password')])  # It's better to compare it with 'new_password' 
+                                         validators=[DataRequired(), EqualTo('new_password')])
     submit = SubmitField('Change Password')
--- veggienosh/routes.py
+++ veggienosh/routes.py
@@ -1,106 +1,124 @@
-# --- Imports ---
-from flask import (render_template, url_for, flash, redirect, 
-                   request, session)
+from flask import render_template, url_for, flash, redirect, request, session
+from veggienosh import app, mongo
 from werkzeug.security import generate_password_hash, check_password_hash
+from veggienosh.forms import RegisterForm, LoginForm, ChangeUsernameForm, ChangePasswordForm
+from flask_pymongo import PyMongo, pymongo
 from bson.objectid import ObjectId
 
-from veggienosh import app, mongo
-from veggienosh.forms import RegisterForm, LoginForm
-
-# --- MongoDB Collections Initialization ---
+# MongoDB Collections Initialization
 users_coll = mongo.db.users
 recipes_coll = mongo.db.recipes
-categories_coll = mongo.db.categories
+categories_coll = mongo.db.catergories
 diets_coll = mongo.db.diets
 dishes_coll = mongo.db.dishes
 
-# --- Routes ---
-
+# Landing page
 @app.route('/')
 @app.route("/home")
 def home():
-    """Serve the landing page."""
-    return render_template(
-        'home.html', title='Home', recipes=recipes_coll.find())
+    return render_template('home.html', title='Veggienosh Home')
 
+# Display all recipes
 @app.route('/all_recipes')
 def all_recipes():
-    """Serve page showing all vegan recipes."""
-    return render_template(
-        "all_recipes.html", recipes=recipes_coll.find(), title='All Vegan Recipes')
+    return render_template("all_recipes.html", recipes=recipes_coll.find(), title='All Veggie Delights')
 
+# Display personal recipes
 @app.route('/my_recipes')
 def my_recipes():
-    """Serve the user's personal recipe page."""
-    return render_template("my_recipes.html", title='My Recipes')
+    return render_template("my_recipes.html", title='My Veggie Creations')
 
+# Add a new recipe
 @app.route('/add_recipe')
 def add_recipe():
-    """Serve the add new recipe page."""
-    return render_template("add_recipe.html", title='Add New Vegan Recipe')
+    return render_template("add_recipe.html", title='Craft a New Veggie Recipe')
 
+# User Login
 @app.route("/login", methods=['GET', 'POST'])
 def login():
-    """User login functionality."""
+    # Check if the user is already logged in
     if 'username' in session:
-        flash('You are already logged in, dear Vegan!')
+        flash('You are already cultivating vegan wonders!')
         return redirect(url_for('home'))
 
     form = LoginForm()
+    users = users_coll  
+
     if form.validate_on_submit():
-        registered_user = users_coll.find_one(
-            {'username': request.form['username']})
-        
-        if (registered_user and 
-                check_password_hash(registered_user['password'], 
-                                    request.form['password'])):
+        registered_user = users.find_one({'username': request.form['username']})
+
+        # Authenticate user
+        if registered_user and check_password_hash(registered_user['password'], request.form['password']):
             session['username'] = request.form['username']
-            flash('Welcome back to VeggieNosh!')
+            flash('Welcome back, Veggie Chef!')
             return redirect(url_for('home'))
         else:
-            flash('Incorrect credentials. Please try again.')
+            flash("Oops, that's not right. Please double-check your credentials.")
             return redirect(url_for('login'))
 
-    return render_template('login.html', form=form, title='Login')
+    return render_template('login.html', form=form, title='Enter the Veggie World')
 
+
+# User Registration
 @app.route("/register", methods=['GET', 'POST'])
 def register():
-    """User registration functionality."""
     if 'username' in session:
-        flash('You are already registered and logged in!')
+        flash('You are already a member of our vegan community!')
         return redirect(url_for('home'))
 
     form = RegisterForm()
+    users = users_coll  
+
     if form.validate_on_submit():
-        registered_user = users_coll.find_one(
-            {'username': request.form['username']})
+        registered_user = users.find_one({'username': request.form['username']})
+
+
         if registered_user:
-            flash('Username taken. Please choose another!')
+            flash("This chef name is already in use. Choose another!")
             return redirect(url_for('register'))
+        else:
+            hashed_password = generate_password_hash(request.form['password'])
+            new_user = {
+                "username": request.form['username'],
+                "password": hashed_password,
+                "user_recipes": [],
+            }
+            users_coll.insert_one(new_user)
+            session["username"] = request.form['username']
+            flash('Welcome to the Veggienosh family!')
+            return redirect(url_for('home'))
 
-        hashed_password = generate_password_hash(request.form['password'])
-        new_user = {
-            "username": request.form['username'],
-            "password": hashed_password,
-            "user_recipes": []
-        }
-        users_coll.insert_one(new_user)
-        session["username"] = request.form['username']
-        flash('Welcome to VeggieNosh! Account created.')
-        return redirect(url_for('home'))
+    return render_template('register.html', form=form, title='Join the Veggie Delights Family')
 
-    return render_template('register.html', form=form, title='Register')
-
+# User Logout
 @app.route("/logout")
 def logout():
-    """Log the user out and redirect to homepage."""
     session.pop("username", None)
-    flash('You have been logged out. See you soon, Vegan!')
+    flash('See you soon, Veggie Chef!')
     return redirect(url_for("home"))
 
+# User Account Settings
 @app.route("/account_settings/<username>")
 def account_settings(username):
-    username = users_coll.find_one(
-        {'username': session['username']})['username']
-    return render_template(
-        'account_settings.html', username=username, title='Account Settings')
+    username = users_coll.find_one({'username': session['username']})['username']
+    return render_template('account_settings.html', username=username, title='Veggie Chef Settings')
+
+# Change Username
+@app.route("/change_username/<username>", methods=['GET', 'POST'])
+def change_username(username):
+    form = ChangeUsernameForm()
+    users = users_coll  
+
+    if form.validate_on_submit():
+        registered_user = users_coll.find_one({'username': request.form['new_username']})
+
+        if registered_user:
+            flash('Chef name already taken. Choose a different one.')
+            return redirect(url_for('change_username', username=session["username"]))
+        else:
+            users_coll.update_one({"username": username}, {"$set": {"username": request.form["new_username"]}})
+            flash("Chef name updated. Please sign in with your new name.")
+            session.pop("username", None)
+            return redirect(url_for("login"))
+
+    return render_template('change_username.html', username=session["username"], form=form, title='Change Chef Name')
