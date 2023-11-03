6ff2a82f6d05eee4190b8027244dbb3d32c67b29
Ire <123773847+irebernardidev@users.noreply.github.com>
2023-11-03 16:11:58 +0000

    Enhanced clarity in JavaScript setup: Documented UI components' initialization, detailed select validation via StackOverflow solution, reordered tooltip and modal activation, and improved home button's hover effect with scaling.

--- veggienosh/static/js/veggienosh.js
+++ veggienosh/static/js/veggienosh.js
@@ -1,9 +1,39 @@
 $(document).ready(function() {
-    $('.sidenav').sidenav(); 
+    // Initialize side navigation for mobile views
+    $('.sidenav').sidenav();
+
+    // Activate dropdown elements with MaterializeCSS
     $(".dropdown-trigger").dropdown();
+
+    // Enhance 'select' elements with MaterializeCSS styling
     $('select').formSelect();
-    $("select[required]").css({display: "block", height: 0, padding: 0, width: 0, position: "absolute"});
-    $('textarea#recipe_description,input#recipe_name').characterCounter();
+
+    // Ensure dropdowns have a valid selection before submitting
+    // (Solution from StackOverflow: https://stackoverflow.com/questions/34248898/how-to-validate-select-option-for-a-materialize-dropdown)
+    $("select[required]").css({
+        display: "block",
+        height: 0,
+        padding: 0,
+        width: 0,
+        position: "absolute"
+    });
+
+    // Enable character counter for inputs with limited text length
+    $('textarea#recipe_description, input#recipe_name').characterCounter();
+
+    // Initialize tooltips for elements with additional information on hover
+    $('.tooltipped').tooltip();
+
+    // Attach modal functionality to modal-trigger elements
     $('.modal').modal();
 
- });
+    // Button visual feedback: Reset to original size when mouseout
+    $("#btn-home").mouseout(function() {
+        $(this).css("transform", "scale(1)");
+    });
+
+    // Button visual feedback: Enlarge slightly when mouseover
+    $("#btn-home").mouseover(function() {
+        $(this).css("transform", "scale(1.05)");
+    });
+});
